f889a099617287b661cdf0dca8b8ed2a1f9c6f21
katakatataan <31636605+katakatataan@users.noreply.github.com>
2021-11-24 02:02:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,15 @@
 const express = require('express');
 
 const Redis = require("ioredis");
-
-const cluster = new Redis.Cluster([
-  {
-    port: 6379,
-    host: "127.0.0.1",
-  },
-  {
-    port: 6381,
-    host: "127.0.0.1",
-  },
-]);
+const nodes = [{
+host: process.env.REDIS_CLUSTER_PRIMARY_ENDPOINT,
+port: '6379',
+}];
+const options = {
+    redisOptions: {
+        tls: {}
+    }
+}
 
 
 // Constants
@@ -23,12 +21,21 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  cluster.set("foo", "bar");
-  cluster.get("foo", (err, res) => {
-    // res === 'bar'
+  const cluster = new Redis.Cluster(nodes,options);
+
+  cluster.set('test-key', 'test-value');
+
+  cluster.get('test-key', function (err, res) {
+    console.log(res);
+
+    if (err) {
+        console.error(err)
+    }
+      
+    cluster.disconnect()
+    res.send(res);
   });
 
-  res.send('Hello World');
 });
 
 app.listen(PORT, HOST);
